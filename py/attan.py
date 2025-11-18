078bc9c7c578704c6b2f3c73889348d41ceb7edf
Waksaker <147053516+Waksaker@users.noreply.github.com>
2025-11-18 08:17:27 +0800

    update  18112025
    update attandance

--- py/attan.py
+++ py/attan.py
@@ -1,52 +1,28 @@
 import mysql.connector
-from datetime import datetime, timedelta
-import time
 
 def connect():
     return mysql.connector.connect(
-        host="localhost",
-        user="mraserver",
-        password="mraglobal2525",
-        database="mraweb"
+        host='localhost',
+        user='root',
+        password='',
+        database='mraweb'
     )
 
-def list_attendance():
+try:
+    print("Connecting to DB...")
     db = connect()
-    cursor = db.cursor()
+    print("Connected to DB!")
+except mysql.connector.Error as err:
+    print("Error connecting to DB:", err)
+    exit(1)
 
-    # SQL query untuk ambil semua data dari table attendance
-    cursor.execute("SELECT * FROM attandance")
+cursor = db.cursor()
+cursor.execute("SELECT dateattan FROM mra_staff")
+results = cursor.fetchall()
+print("Rows fetched:", len(results))
 
-    # Ambil semua baris hasil query
-    results = cursor.fetchall()
+for row in results:
+    print("DATE ATTENDANCE:", row[0])
 
-    # Tutup cursor dan connection
-    cursor.close()
-    db.close()
-
-    return results
-
-def attan():
-    
-
-def main():
-    while True:
-        # Contoh penggunaan fungsi
-        attendance_list = list_attendance()
-        if attendance_list:
-            for row in attendance_list:
-                name = row[1]
-                times = row[4]
-                date = row[5]
-                hari_ini = datetime.today()
-                semalam = hari_ini - timedelta(days=1)
-                formatted_date = semalam.strftime("%Y-%m-%d")
-
-                print(f"NAME: {name}, TIME: {times}, DATE: {date}, TARIKH SEMALAM: {formatted_date}")
-        else:
-            print("Tiada data.")
-
-        time.sleep(1)
-
-if __name__ == "__main__":
-    main()
+cursor.close()
+db.close()
